cd7bbef306058ae9e1fc154f84be4bac92c08801
Ivan <ivankrivko085@gmail.com>
2025-08-08 11:06:43 +0300

    last commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav, {});
+
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal, {});
+    
+    var galery = document.querySelectorAll('.materialboxed');
+    var instances_galery = M.Materialbox.init(galery, {});
+
 });
+
